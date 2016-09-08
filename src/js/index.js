ffc4c83c8648b9471c4c4e9107fae78207dc0c31
Emil Nik <emil.nik1002@gmail.com>
2016-09-09 00:17:15 +0300

    Decided to write the library in english....

--- src/js/index.js
+++ src/js/index.js
@@ -1,62 +1,40 @@
 var ewig = "";
-var holder = "";
-function permuter(word, uppers) {
-    var ind = 0,
-        gate = 0,
-        next = 0,
-        count = 0,
-        permuda = ["а", "б", "в", "г", "д", "е", "ж", "з", "и", "й", "к", "л", "м", "н", "о", "п", "р", "с", "т", "у", "ф", "х", "ц", "ч", "ш", "щ", "ъ", "ы", "ь", "э", "ю", "я", " "],
-        word = word.toLowerCase(),
-        polygon = [],
-        ups = uppers.split(',');
-    for (ind = 0; ind < word.length; ind++) {
-        polygon[ind] = word.charCodeAt(ind) - 1072;
-        if (polygon[ind] < 0) {
-            polygon[ind] = 32
-        };
-    }
-
-    if (polygon[polygon.length - 1] < 32) {
-        polygon[polygon.length - 1]++;
-    }
-    else {
-        polygon[polygon.length - 1] = 0;
-        for (ind = polygon.length - 2; ind > -1; ind--) {
-            if (polygon[ind] < 31) {
-                polygon[ind]++; gate++; break;
+var holder ="";
+    function permuter(word) {
+        var ind=0;
+        var gate=0;
+        var next = 0;
+        var permuda = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+        word = word.toLowerCase();
+        var polygon = [];
+        for (ind=0; ind<word.length;ind++) {
+            polygon[ind] = word.charCodeAt(ind) - 97;
+        }
+        if (polygon[polygon.length-1]<25) {
+            polygon[polygon.length-1]++;}
+        else {
+            polygon[polygon.length-1]=0;
+            for (ind = polygon.length-2; ind>-1; ind--){
+                if(polygon[ind]<25) {polygon[ind]++; gate++; break;}
+                else {polygon[ind]=0;}
             }
-            else {
-                polygon[ind] = 0;
-            }
+            if (gate==0) {polygon.unshift(0);}
         }
-
-        if (gate == 0) {
-            polygon.unshift(0);
+        holder = permuda[polygon[0]].toUpperCase();
+        for     (ind = 1; ind < polygon.length; ind++) {
+            holder= holder + permuda[polygon[ind]];
         }
+        return holder;
     }
-
-    holder = "";
-    count = 0;
-    for (ind = 0; ind < polygon.length; ind++) {
-        if (ups[count] == ind) {
-            holder = holder + permuda[polygon[ind]].toUpperCase();
-            count++;
-            continue;
-        }
-
-        holder = holder + permuda[polygon[ind]];
-    }
-
-    return holder;
-}
-
-function permute(eyedee) {
-    var word1 = document.getElementById(eyedee).innerHTML,
-        uppers = document.getElementById(eyedee).dataset.up;
-    ewig = setInterval(function() { document.getElementById(eyedee).innerHTML = permuter(word1, uppers); word1 = holder; }, 1);
-}
-
-function unpermute(term, eyedee) {
-    clearInterval(ewig);
-    document.getElementById(eyedee).innerHTML = term;
-}
+    function permute(eyedee) {
+        var word1=document.getElementById(eyedee).innerHTML;
+        ewig = setInterval(function() {document.getElementById(eyedee).innerHTML = permuter(word1); word1=holder;}, 1);}
+    function unpermute(term,eyedee) {
+        clearInterval(ewig);
+        document.getElementById(eyedee).innerHTML = term;}
+    function hexperm(eyedee) {
+        var word1= "Hex";
+        ewig = setInterval(function() {document.getElementById(eyedee).innerHTML = "Reference " + permuter(word1); word1=holder;}, 1);}
+function arcperm(eyedee) {
+    var word1= "Archives";
+    ewig = setInterval(function() {document.getElementById(eyedee).innerHTML = "Image " + permuter(word1); word1=holder;}, 1);}
